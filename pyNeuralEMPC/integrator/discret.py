fix hessian !

pyNeuralEMPC/integrator/discret.py
@@ -106,9 +106,6 @@ class DiscretIntegrator(Integrator):
         final_H[:,x.shape[1]*x.shape[0]:,  :x.shape[1]*(x.shape[0]-1) ] += model_H[:,  x.shape[1]*x.shape[0]:, x.shape[1]:x.shape[1]*x.shape[0] ]
 
         
-        print("final_H" , model_H)
-
-        print("final_H" , final_H)
         return final_H
 
     def hessianstructure(self, nb_sample=3):
@@ -131,10 +128,24 @@ class DiscretIntegrator(Integrator):
             
             hessian  = self.model.hessian(x_random, u_random, x_random[0,:],p=p_random, tvp=tvp_random)
 
+
+            hessian = hessian.reshape(-1,*hessian.shape[2:])
+            final_hessian = np.zeros_like(hessian)
+            # x_t x x_t 
+            final_hessian[:,:x_random.shape[1]*(x_random.shape[0]-1), 0:x_random.shape[1]*(x_random.shape[0]-1)] += hessian[:,x_random.shape[1]:x_random.shape[1]*x_random.shape[0], x_random.shape[1]:x_random.shape[1]*x_random.shape[0]]
+            # u_t x u_t
+            final_hessian[:,x_random.shape[1]*x_random.shape[0]:, x_random.shape[1]*x_random.shape[0]:] += hessian[:,x_random.shape[1]*x_random.shape[0]:, x_random.shape[1]*x_random.shape[0]:]
+
+            # x_t x u_t
+            final_hessian[:, :x_random.shape[1]*(x_random.shape[0]-1), x_random.shape[1]*x_random.shape[0]:] += hessian[:,x_random.shape[1]:x_random.shape[1]*x_random.shape[0], x_random.shape[1]*x_random.shape[0]:]
+
+            # u_t x x_t
+            final_hessian[:,x_random.shape[1]*x_random.shape[0]:,  :x_random.shape[1]*(x_random.shape[0]-1) ] += hessian[:,  x_random.shape[1]*x_random.shape[0]:, x_random.shape[1]:x_random.shape[1]*x_random.shape[0] ]
+
             if hessian_map is None:
-                hessian_map = (hessian!= 0.0).astype(np.float64)
+                hessian_map = (final_hessian!= 0.0).astype(np.float64)
             else:
-                hessian_map += (hessian!= 0.0).astype(np.float64)
+                hessian_map += (final_hessian!= 0.0).astype(np.float64)
                 hessian_map = hessian_map.astype(np.bool).astype(np.float64)
-        hessian_map  = np.sum(hessian_map, axis=[0,1]).astype(np.bool).astype(np.float64)
+        hessian_map  = np.sum(hessian_map, axis=0).astype(np.bool).astype(np.float64)
         return hessian_map

pyNeuralEMPC/optimizer/ipopt.py
@@ -41,10 +41,10 @@ class IpoptProblem(ProblemInterface):
 
         for ctr in self.constraints_list:
             contraints_forward_list.append(ctr.forward(states, u, p=p, tvp=tvp))
-        print("contraints_forward_list : ", contraints_forward_list)
+
         return np.concatenate(contraints_forward_list)
 
-    """
+    
     def hessianstructure(self):
 
         hessian_map_objective = self.objective_func.hessianstructure(  self.integrator.H, self.integrator.model)
@@ -52,9 +52,9 @@ class IpoptProblem(ProblemInterface):
         hessian_map_integrator = self.integrator.hessianstructure()
 
         final_hessian_map  = (hessian_map_objective + hessian_map_integrator).astype(np.bool).astype(np.float32)
-        
-        return None#np.nonzero(final_hessian_map)
-    """
+
+        return np.nonzero(np.tril(final_hessian_map))#np.nonzero(np.ones_like(final_hessian_map))
+    
 
     
     def hessian(self, x, lagrange, obj_factor):
@@ -68,13 +68,13 @@ class IpoptProblem(ProblemInterface):
         
         # TODO add constraints 
         for idx, lagrange_coef in enumerate(lagrange): # TODO remove this loop (vec comp)
-            print(f"idx : {idx} lagrange_coef= {lagrange_coef} final = {integrator_hessian_matrice[idx]}")
+
             hessian_matrice+=lagrange_coef*integrator_hessian_matrice[idx]
 
-        print("result  : ")
-        print(hessian_matrice)
-        #row, col = self.hessianstructure()
-        return hessian_matrice#[row, col]
+
+        row, col = self.hessianstructure()
+
+        return hessian_matrice[row, col]
 
     def jacobian(self, x):
         states, u, tvp, p = self._split(x)
@@ -155,7 +155,7 @@ class Ipopt(Optimizer):
             cu=cu
             )
 
-        nlp.addOption('max_iter',            1)# self.max_iteration)
+        nlp.addOption('max_iter',            self.max_iteration)# 
         nlp.addOption('derivative_test', 'second-order')
         nlp.addOption('derivative_test_print_all', 'yes')
         nlp.addOption('point_perturbation_radius',1e-1)